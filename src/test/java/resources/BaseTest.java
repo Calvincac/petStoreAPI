1264d656b9bee2e53fe9f34f3176b53b163260a9
Calvin Custodio <calvincustodio@Calvins-MBP.home>
2021-02-27 18:44:52 +0100

    removed imports

--- src/test/java/resources/BaseTest.java
+++ src/test/java/resources/BaseTest.java
@@ -1,14 +1,9 @@
 package resources;
 
 import data.DataBuilder;
-import data.PetStoreTestData;
-import org.testng.annotations.AfterTest;
-import pojo.Pet;
 import service.PetService;
 import service.StoreService;
 import service.UserService;
-
-import java.util.List;
 
 public class BaseTest {
     public PetService petService = new PetService();
