ff5ae0d517033e0b39a1fba11acc6976eed0d99f
Calvin Custodio <calvincustodio@Calvins-MBP.home>
2021-02-27 18:41:15 +0100

    adjusted BaseTest

--- src/test/java/resources/BaseTest.java
+++ src/test/java/resources/BaseTest.java
@@ -15,14 +15,4 @@ public class BaseTest {
     public StoreService storeService = new StoreService();
     public UserService userService = new UserService();
     public DataBuilder dataBuilder = new DataBuilder();
-
-    @AfterTest
-    public void cleanUp() {
-        List<Pet> availablePets = petService.retrievePetByStatus(PetStoreTestData.AVAILABLE_STATUS.toString());
-        availablePets.forEach(pet -> {petService.deleteAPet(pet.getId());});
-        List<Pet> pendingPets = petService.retrievePetByStatus(PetStoreTestData.PENDING_STATUS.toString());
-        pendingPets.forEach(pet -> {petService.deleteAPet(pet.getId());});
-        List<Pet> soldPets = petService.retrievePetByStatus(PetStoreTestData.SOLD_STATUS.toString());
-        soldPets.forEach(pet -> {petService.deleteAPet(pet.getId());});
-    }
 }
